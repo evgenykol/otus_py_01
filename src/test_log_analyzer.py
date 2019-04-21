e8c717d1dfdcfed5eda98e1e15b8c3b226fd2825
evgenyko <edkolotilov@gmail.com>
2019-04-21 22:48:55 +0300

    Добавил тестов для переработаного функционала

--- src/test_log_analyzer.py
+++ src/test_log_analyzer.py
@@ -5,6 +5,7 @@ import datetime as dt
 import log_analyzer as la
 from unittest import mock
 
+
 class LogAnalyzerTests(unittest.TestCase):
 
     config = {
@@ -15,9 +16,97 @@ class LogAnalyzerTests(unittest.TestCase):
         "SUCSESSFUL_PERCENT": "0.95"
     }
 
+    DEFAULT_CONFIG_PATH = './log_analyzer.cfg'
+
     line = '1.199.4.96 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/slot/4705/groups HTTP/1.1" 200 2613 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-3800516057-4708-9752745" "2a828197ae235b0b3cb" 0.704'
 
-    def test_find_last_log_ok(self):
+    def test__read_congfig_ok(self):
+        def side_effect(arg):
+            if(arg == LogAnalyzerTests.DEFAULT_CONFIG_PATH):
+                return True
+            else:
+                return False
+
+        patcher = mock.patch('os.path.exists')
+        mock_thing = patcher.start()
+        mock_thing.side_effect = side_effect
+
+        with mock.patch('json.loads') as jl:
+            jl.return_value = LogAnalyzerTests.config
+            self.assertDictEqual(la.read_config(LogAnalyzerTests.DEFAULT_CONFIG_PATH), LogAnalyzerTests.config)
+
+    def test__read_congfig_cfg_not_found(self):
+        def side_effect(arg):
+                return False
+
+        patcher = mock.patch('os.path.exists')
+        mock_thing = patcher.start()
+        mock_thing.side_effect = side_effect
+
+        self.assertRaises(FileNotFoundError, la.read_config, LogAnalyzerTests.DEFAULT_CONFIG_PATH)
+
+    def test__init_config_default(self):
+        ext = {"REPORT_SIZE": 1234}
+        with mock.patch('log_analyzer.read_config') as rc:
+            ret = LogAnalyzerTests.config.copy()
+            ret.update(ext)
+            rc.side_effect = [LogAnalyzerTests.config, None]
+
+            self.assertDictEqual(la.init_config(None), LogAnalyzerTests.config)
+
+    def test__init_config_external(self):
+        ext = {"REPORT_SIZE": 1234}
+        with mock.patch('log_analyzer.read_config') as rc:
+            ret = LogAnalyzerTests.config.copy()
+            ret.update(ext)
+            rc.side_effect = [LogAnalyzerTests.config, ext]
+
+            self.assertDictEqual(la.init_config(LogAnalyzerTests.DEFAULT_CONFIG_PATH), ret)
+
+    def test__check_preconditions_ok(self):
+        def side_effect(arg):
+            if(arg == "./report.html"):
+                return True
+            else:
+                return False
+
+        patcher = mock.patch('os.path.exists')
+        mock_thing = patcher.start()
+        mock_thing.side_effect = side_effect
+
+        with mock.patch('os.mkdir') as md:
+            la.check_preconditions(LogAnalyzerTests.config)
+            self.assertEqual(md.call_count, 1)
+
+    def test__check_preconditions_no_template(self):
+        def side_effect(arg):
+                return False
+
+        patcher = mock.patch('os.path.exists')
+        mock_thing = patcher.start()
+        mock_thing.side_effect = side_effect
+
+        self.assertRaises(FileNotFoundError,
+                          la.check_preconditions,
+                          LogAnalyzerTests.config
+                          )
+
+    def test__check_preconditions_report_dir_exists(self):
+        def side_effect(arg):
+            if(arg == "./report.html" or arg == "../reports"):
+                return True
+            else:
+                return False
+
+        patcher = mock.patch('os.path.exists')
+        mock_thing = patcher.start()
+        mock_thing.side_effect = side_effect
+
+        with mock.patch('os.mkdir') as md:
+            la.check_preconditions(LogAnalyzerTests.config)
+            self.assertEqual(md.call_count, 0)
+
+    def test__find_last_log_ok(self):
         with mock.patch('os.listdir') as mock_listdir:
             with mock.patch('os.path.isdir') as mock_isdir:
                 mock_listdir.return_value = [
@@ -37,7 +126,32 @@ class LogAnalyzerTests(unittest.TestCase):
 
                 self.assertEqual(last_log.date, dt.datetime(2018, 6, 28))
 
-    def test_find_last_log_no_logfile(self):
+    def test__find_last_log_bad_date(self):
+        with mock.patch('os.listdir') as mock_listdir:
+            with mock.patch('os.path.isdir') as mock_isdir:
+                mock_listdir.return_value = [
+                    'bar1',
+                    'nginx-access-ui.log-20174242',
+                ]
+                mock_isdir.side_effect = [True]
+
+                self.assertRaises(FileNotFoundError,
+                                  la.find_last_log,
+                                  LogAnalyzerTests.config.get('LOG_DIR'),
+                                  )
+
+    def test__find_last_log_no_logdir(self):
+        with mock.patch('os.listdir') as mock_listdir:
+            with mock.patch('os.path.isdir') as mock_isdir:
+                mock_listdir.return_value = []
+                mock_isdir.side_effect = [False]
+
+                self.assertRaises(FileNotFoundError,
+                                  la.find_last_log,
+                                  LogAnalyzerTests.config.get('LOG_DIR'),
+                                  )
+
+    def test__find_last_log_no_logfile(self):
         with mock.patch('os.listdir') as mock_listdir:
             with mock.patch('os.path.isdir') as mock_isdir:
                 mock_listdir.return_value = []
@@ -48,7 +162,7 @@ class LogAnalyzerTests(unittest.TestCase):
                                   LogAnalyzerTests.config.get('LOG_DIR'),
                                   )
 
-    def test_make_report_path_ok(self):
+    def test__make_report_path_ok(self):
         def side_effect(arg):
             if(arg == './report.html' or
                arg == '../reports'
@@ -67,55 +181,21 @@ class LogAnalyzerTests(unittest.TestCase):
         path = la.make_report_path(LogAnalyzerTests.config, last_log)
         self.assertEqual(path, '../reports/report-2018.06.29.html')
 
-    def test_make_report_path_no_template(self):
-        def side_effect(arg):
-                return False
-
-        patcher = mock.patch('os.path.exists')
-        mock_thing = patcher.start()
-        mock_thing.side_effect = side_effect
-
-        last_log = collections.namedtuple('last_log', ['date', 'path'])
-        last_log.date = dt.datetime(2018, 6, 28)
-
-        self.assertRaises(FileNotFoundError,
-                          la.make_report_path, LogAnalyzerTests.config,
-                          last_log
-                          )
-
-    def test_make_report_path_already_exists(self):
-        def side_effect(arg):
-            if(arg == './report.html' or
-               arg == '../reports/report-2018.06.28.html'
-               ):
-                return True
-            else:
-                return False
-
-        patcher = mock.patch('os.path.exists')
-        mock_thing = patcher.start()
-        mock_thing.side_effect = side_effect
-
-        last_log = collections.namedtuple('last_log', ['date', 'path'])
-        last_log.date = dt.datetime(2018, 6, 28)
-
-        self.assertRaises(FileExistsError,
-                          la.make_report_path, LogAnalyzerTests.config,
-                          last_log
-                          )
-
-    def test_process_line_ok(self):
+    def test__process_line_ok(self):
         out = la.process_line(LogAnalyzerTests.line, 42)
         self.assertEqual(out, {'url': "/api/v2/slot/4705/groups",
                                'request_time': 0.704
                                }
                          )
 
-    def test_process_line_nok(self):
+    def test__process_line_nok(self):
         line = ''
         self.assertFalse(la.process_line(line, 123))
 
-    def test_collect_url_data(self):
+    def test__process_line_bad_url(self):
+        self.assertFalse(la.process_line('0 1 2 3 4 5 6 7 8 9 10 11', 42))
+
+    def test__collect_url_data(self):
         reader = [{'url': "/url1", 'request_time': 1},
                   {'url': "/url1", 'request_time': 2},
                   {'url': "/url1", 'request_time': 3},
@@ -127,7 +207,7 @@ class LogAnalyzerTests(unittest.TestCase):
         self.assertEqual(urls.total_time, 10)
         self.assertEqual(urls.urls, {'/url2': [4], '/url1': [1, 2, 3]})
 
-    def test_calc_statistic(self):
+    def test__calc_statistic(self):
         Urls = collections.namedtuple('Urls', ['urls', 'count'])
         Urls.urls = collections.defaultdict(list)
 
